79c828ef11007b005e1a3e2eaf5e0288a9b28301
unknown <unwanted.akash@gmail.com>
2023-04-26 05:23:13 +0600

    Complete the portfolio

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <main className="mx-auto flex w-full  max-w-3xl flex-1">
+      <div className="px-6 py-64 text-center md:px-0 md:py-52 md:text-left">
+        <h1 className="pb-4 text-4xl font-bold text-white md:text-6xl">
+          Md. Asaduzzaman Akash
+        </h1>
+        <p className="text-xl font-semibold text-white md:pb-1 md:text-3xl">
+          Front End Developer & Coffee Lover
+        </p>
+        <p className="text-lg  text-stone-300  md:text-xl">
+          As a front-end web developer with a specialization in React, I
+          have a keen eye for creating visually stunning and highly responsive
+          user interfaces. With a natural talent for problem solving and an
+          ability to quickly learn new technologies, I am always pushing myself
+          to stay ahead of the curve.
+        </p>
+      </div>
+    </main>
+  );
+}
